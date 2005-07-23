b9c49dbc80e07564e24720859f459d1689f48e74
treectrl <treectrl>
2005-07-23 00:47:08 +0000

    Bump version to 2.1.

--- shellicon/pkgIndex.tcl
+++ shellicon/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {[catch {package require Tcl 8.4}]} return
-package ifneeded shellicon 2.0.1 [list load [file join $dir shellicon20.dll] shellicon]
+package ifneeded shellicon 2.1 [list load [file join $dir shellicon21.dll] shellicon]
